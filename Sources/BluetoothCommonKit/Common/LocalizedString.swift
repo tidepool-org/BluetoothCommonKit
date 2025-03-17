3529110f5a26d75858a19ddfb74d4a8bc3d03309
Nathaniel Hamming <nathanielhamming@Nathaniels-MacBook-Pro-2023.local>
2025-03-17 13:29:19 -0300

    added README

--- Sources/BluetoothCommonKit/Common/LocalizedString.swift
+++ Sources/BluetoothCommonKit/Common/LocalizedString.swift
@@ -12,7 +12,7 @@ private class FrameworkBundle {
     static let main = Bundle(for: FrameworkBundle.self)
 }
 
-func LocalizedString(_ key: String, tableName: String? = nil, value: String? = nil, comment: String) -> String {
+public func LocalizedString(_ key: String, tableName: String? = nil, value: String? = nil, comment: String) -> String {
     if let value = value {
         return NSLocalizedString(key, tableName: tableName, bundle: FrameworkBundle.main, value: value, comment: comment)
     } else {
